333198e3aa3a055dbf7de811c78a98f61735a7cc
Bekzod <bekzodturgunoff@gmail.com>
2022-07-18 15:16:08 +0500

    first commit, not working yet

---
+++ src/components/search/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a1a175b76amsh1ebc226108746a2p1fb74fjsn44021b5c6937",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL =
+  "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+
